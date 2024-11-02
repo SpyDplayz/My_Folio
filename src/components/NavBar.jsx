525bf5a8f9f6be44cd1a0be428f4b2c671a14dac
Alay Gosai <95856215+SpyDplayz@users.noreply.github.com>
2024-11-02 08:46:35 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
   return (
     <nav className='mb-20 flex item-center justify-between py-6'>
         <div className='flex flex-shrink-0 items-center'>
-            <img className='mx-20 w-10' src={logo1} alt="logo" />
+            //<img className='mx-20 w-10' src={logo1} alt="logo" />
         </div>
         <div className='m-1 mx-20  flex items-center justify-center gap-4 text-2xl'>
             <a href="https://in.linkedin.com/in/alay-gosai-40620120a"><FaLinkedin /></a>
